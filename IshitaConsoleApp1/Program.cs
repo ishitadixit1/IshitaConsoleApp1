cef2083f5e4fb0136bf0876118222a1a5b61970f
ishitadixit1 <162412818+ishitadixit1@users.noreply.github.com>
2024-04-10 15:29:50 +0530

    Add project files.

---
+++ IshitaConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IshitaConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the first number :");
+            double firstnum= Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter the second number :");
+            double secondnum = Convert.ToDouble(Console.ReadLine());
+            double sum = firstnum + secondnum;
+            Console.WriteLine($"The sum of {firstnum} and {secondnum} is: {sum}");
+
+        }
+    }
+}
